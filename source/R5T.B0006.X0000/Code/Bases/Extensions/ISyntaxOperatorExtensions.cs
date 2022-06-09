d4026a23ef7f5ada6fe27487c2786ea3d743823a
davidcoats <david@getsafetycone.com>
2022-06-09 12:16:51 -0700

    Interim changes.

---
+++ source/R5T.B0006.X0000/Code/Bases/Extensions/ISyntaxOperatorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.B0006;
+
+
+namespace System
+{
+    public static class ISyntaxOperatorExtensions
+    {
+        public static int GetIndexForPublicAccessModifer(this ISyntaxOperator _,
+            SyntaxTokenList modifiers)
+        {
+            var output = modifiers.GetIndexForPublicAccessModifer();
+            return output;
+        }
+    }
+}
